11ca25e62250c0dffb329ba7968a36e6ec05a361
Asif Iqbal Rimon <ariyan8185@gmail.com>
2023-01-02 20:42:50 +0600

    stripe payment method

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express();
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 dotenv.config();
-
+const cors = require("cors")
 //  api route
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/auth')
 const productRouter = require('./routes/product')
 const cartRouter = require('./routes/cart')
-const orderRouter = require('./routes/order')
+const orderRouter = require('./routes/order');
+const stripeRouter = require("./routes/stripe")
 
 //  server port
 const port = process.env.PORT || 5000;
@@ -23,12 +24,13 @@ mongoose.connect(process.env.MONGO_URI)
     });
 app.use(express.json());
 
-
+app.use(cors())
 app.use("/api/auth", authRouter)
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 app.use('/api/orders', orderRouter);
+app.use('/api/checkout' , stripeRouter)
 
 app.listen(port, () => {
     console.log('backend server is running');
